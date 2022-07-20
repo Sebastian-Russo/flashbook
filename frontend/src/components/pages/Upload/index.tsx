ecfcd6862cd810a1cb07353c89ab708b7948af56
Sebastian Russo <russo.sebastian@gmail.com>
2022-07-19 21:30:20 -0600

    Added text documents. Added slight styling upload page.

--- frontend/src/components/pages/Upload/index.tsx
+++ frontend/src/components/pages/Upload/index.tsx
@@ -16,6 +16,8 @@ const Upload: React.FC<UploadProps> = props => {
 	const [selected_file, setSelectedFile] = React.useState<SelectedFile>();
 	const [is_selected, setIsSelected] = React.useState(false);
 
+	console.log('Selected file:', selected_file);
+
 	const changeHandler = (event:any) => {
 		setSelectedFile(event.target.files[0]); // this an object that contains the details of files selected to be uploaded in a form
 		setIsSelected(true);
@@ -23,47 +25,50 @@ const Upload: React.FC<UploadProps> = props => {
 
 	// TODO: Send to AWS ( S3 || Amazon Textract)
 	const handleSubmission = () => {
-	// 	const formData = new FormData();
+		const formData = new FormData();
 
-	// 	formData.append('File', selected_file);
+		formData.append('File', selected_file);
 
-	// 	fetch(
-	// 		'https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>',
-	// 		{
-	// 			method: 'POST',
-	// 			body: formData,
-	// 		}
-	// 	)
-	// 		.then((response) => response.json())
-	// 		.then((result) => {
-	// 			console.log('Success:', result);
-	// 		})
-	// 		.catch((error) => {
-	// 			console.error('Error:', error);
-	// 		});
+		fetch(
+			'https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>',
+			{
+				method: 'POST',
+				body: formData,
+			}
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log('Success:', result);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
 	};
 
 
 	return (
-		<RB.Flex>
-			<div>Upload Page</div>
-			<input type="file" name="file" onChange={changeHandler} />
-			{is_selected ? (
-				<div>
-					<p>Filename: {selected_file.name}</p>
-					<p>Filetype: {selected_file.type}</p>
-					<p>Size in bytes: {selected_file.size}</p>
-					<p>
-						lastModifiedDate:{' '}
-						{selected_file.lastModifiedDate.toLocaleDateString()}
-					</p>
-				</div>
-			) : (
-				<p>Select a file to show details</p>
-			)}
-			<div>
-				<button onClick={handleSubmission}>Submit</button>
-			</div>
+		<RB.Flex justifyContent="center" alignItems="center">
+			<RB.Flex flexDirection="column">
+
+				<input type="file" name="file" onChange={changeHandler} />
+				{is_selected ? (
+					<RB.Flex flexDirection="column">
+						<p>Filename: {selected_file.name}</p>
+						<p>Filetype: {selected_file.type}</p>
+						<p>Size in bytes: {selected_file.size}</p>
+						<p>
+							lastModifiedDate:{' '}
+							{selected_file.lastModifiedDate.toLocaleDateString()}
+						</p>
+					</RB.Flex>
+				) : (
+					<p>Select a file to show details</p>
+				)}
+				<RB.Flex>
+					<button onClick={handleSubmission}>Submit</button>
+				</RB.Flex>
+			
+			</RB.Flex>
 		</RB.Flex>
 	)
 }
